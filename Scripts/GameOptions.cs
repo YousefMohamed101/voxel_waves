d857e941147dce0ad175ae7812ac36314974a050
YousefMohamed101 <thehappyboy28@gmail.com>
2024-08-09 23:55:31 +0800

    Implemented Game settings and setting save between scenes and added new settings

--- Scripts/GameOptions.cs
+++ Scripts/GameOptions.cs
@@ -6,8 +6,17 @@ public partial class GameOptions : Node
 	public float M_Slider;
 	public float BGM_Slider;
 	public float ES_Slider;
+	public int slide_button;
+	public int Button_state;
+	public int Checked_state;
+	private int count;
+
 	public override void _Ready()
 	{
+		Checked_state = 0;
+		count = 0;
+		Button_state = 0;
+		slide_button = 0;
 		M_Slider = 0;
 		BGM_Slider = 0;
 		ES_Slider = 0;
@@ -30,6 +39,41 @@ public partial class GameOptions : Node
 		{
 			ES_Slider = m;
 			AudioServer.SetBusVolumeDb(2, m);
+		}
+
+	}
+	public void ButtonStateToggled(int x)
+	{
+		Button_state = x;
+	}
+	public void ButtonCheckedMe()
+	{
+		if (Checked_state == 0)
+		{
+			Checked_state = 1;
+		}
+		else
+		{
+			Checked_state = 0;
+		}
+
+
+		switch (Checked_state)
+		{
+			case 0:
+				DisplayServer.WindowSetMode(DisplayServer.WindowMode.Windowed);
+				GD.Print("windowed");
+
+				break;
+
+			case 1:
+				DisplayServer.WindowSetMode(DisplayServer.WindowMode.Fullscreen);
+				GD.Print("fullscreen");
+				break;
+
+			default:
+				DisplayServer.WindowSetMode(DisplayServer.WindowMode.Windowed);
+				break;
 		}
 
 	}
